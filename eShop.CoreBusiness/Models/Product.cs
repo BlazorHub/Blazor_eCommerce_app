88e6208f252a62ba6331a65cabf04319c6ee31ae
Vieriu Alexandru <vieriu.alexandru.25@gmail.com>
2021-01-29 17:21:54 +0200

    51. Order Summary Component

--- eShop.CoreBusiness/Models/Product.cs
+++ eShop.CoreBusiness/Models/Product.cs
@@ -1,7 +1,7 @@
 ﻿namespace eShop.CoreBusiness.Models
 {
     public class Product
-    {
+    {        
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
