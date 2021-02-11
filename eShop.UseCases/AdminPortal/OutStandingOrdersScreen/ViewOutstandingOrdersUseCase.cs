9fdba5f0215ffc242795a1b45e8517002c304d99
Vieriu Alexandru <vieriu.alexandru.25@gmail.com>
2021-02-11 15:24:30 +0200

    Admin Portal

---
+++ eShop.UseCases/AdminPortal/OutStandingOrdersScreen/ViewOutstandingOrdersUseCase.cs
@@ -0,0 +1,22 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System.Collections.Generic;
+
+namespace eShop.UseCases.AdminPortal.OutStandingOrdersScreen
+{
+    public class ViewOutstandingOrdersUseCase : IViewOutstandingOrdersUseCase
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public ViewOutstandingOrdersUseCase(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public IEnumerable<Order> Execute()
+        {
+            return _orderRepository.GetOutstadingOrders();
+        }
+
+    }
+}
